3c240e3838f201dacffed1a18e34f76aae30c49e
Jesse Farmer <jessethebuilder@yahoo.com>
2015-08-09 18:19:22 -0700

    All

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,4 +11,27 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_base_class_for_anonymous_controllers = false
   config.order = "random"
+  
+  config.include FactoryGirl::Syntax::Methods
+  config.include ActionView::Helpers::NumberHelper
+
+  # config.require 'database_cleaner'
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with :deletion
+  end
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+  config.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+
+  config.include RequestSpecHelper
+
 end
